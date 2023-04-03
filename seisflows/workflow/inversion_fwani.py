3685d361af4dba4166f754e45d5bbd0958aca92c
Eduardo Valero Cano <eduardo.valerocano@kaust.edu.sa>
2023-04-03 19:38:59 +0300

    added new workflow: inversion full waveform ambient noise inversion (fwani)

---
+++ seisflows/workflow/inversion_fwani.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+A workflow similar to Inversion but for ambient noise simulations
+"""
+
+from seisflows.workflow.inversion import Inversion
+from seisflows.workflow.migration_noise import MigrationNoise
+
+
+class InversionFwani(Inversion, MigrationFwani):
+    """
+    Inversion Noise Workflow
+    ----------------
+    Dummy
+
+    Parameters
+    ----------
+    :type dummy:
+    :param dummy
+
+    Paths
+    -----
+    :type dummy:
+    :param dummy:
+    ***
+    """
+    __doc__ = Inversion.__doc__ + __doc__
+
+    def __init__(self, modules=None, **kwargs):
+        """
+        Instantiate InversionNoise-specific parameters
+
+        :type dummy:
+        :param dummy:
+        """
+        super().__init__(**kwargs)
+
+        self._modules = modules
