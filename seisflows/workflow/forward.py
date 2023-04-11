a73c52c287734f5553a4173fea3569867c816056
Eduardo Valero Cano <eduardo.valerocano@kaust.edu.sa>
2023-04-11 16:04:07 +0200

    Residuals log improvements and line search fixes (#1)
    * residuals are now written on one file per source
    
    * fixed: try model was not being exposed to the solver during line search
    
    * export_residuals is now working
    
    * changed naming of residuals files
    
    * fixed: exported traces were overwritten when export_traces=True

--- seisflows/workflow/forward.py
+++ seisflows/workflow/forward.py
@@ -340,7 +340,7 @@ class Forward:
 
         self.system.run(run_list, path_model=self.path.model_init,
                         save_residuals=os.path.join(self.path.eval_grad,
-                                                    "residuals.txt")
+                                                    "residuals_{src}_{it}.txt")
                         )
 
     def prepare_data_for_solver(self, **kwargs):
@@ -427,10 +427,19 @@ class Forward:
                          "objective function")
             return
 
+        save_residuals = save_residuals.format(src=self.solver.source_name,
+                                               it="1")
+
+        if self.export_residuals:
+            export_residuals = os.path.join(self.path.output, "residuals")
+        else:
+            export_residuals = False
+
         logger.debug(f"quantifying misfit with "
                      f"'{self.preprocess.__class__.__name__}'")
         self.preprocess.quantify_misfit(
             source_name=self.solver.source_name,
             save_adjsrcs=os.path.join(self.solver.cwd, "traces", "adj"),
-            save_residuals=save_residuals
+            save_residuals=save_residuals,
+            export_residuals=export_residuals
         )
